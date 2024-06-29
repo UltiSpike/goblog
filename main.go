5e9b0cd59c5dc745b260a445f7c2dab0bf4b9476
王磊 <wanglei39@corp.netease.com>
2024-06-29 17:09:17 +0800

    尝试 http.ServceMuxï

--- main.go
+++ main.go
@@ -5,25 +5,32 @@ import (
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if r.URL.Path == "/" {
 		_, err := fmt.Fprint(w, "Hello~ love me")
 		if err != nil {
 			return
 		}
-	} else if r.URL.Path == "/about" {
-		fmt.Fprint(w, "此博客是用以记录编程笔记，请联系"+
-			"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
 	} else {
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(w, "no pages")
 	}
 }
 
-func main() {
+func otherHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	_, err := fmt.Fprint(w, "此博客是用以记录编程笔记，请联系"+
+		"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
+	if err != nil {
+		return
+	}
+}
 
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":3000", nil)
+func main() {
+	router := http.NewServeMux()
+	router.HandleFunc("/", defaultHandler)
+	router.HandleFunc("/about", otherHandler)
+	http.ListenAndServe(":3000", router)
 
 }
