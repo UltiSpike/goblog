138a63c1dec4f50e958205cf0ba9a9a0a1073db7
王磊 <wanglei39@corp.netease.com>
2024-06-29 15:53:04 +0800

    add new pages

--- main.go
+++ main.go
@@ -5,10 +5,23 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		_, err := fmt.Fprint(w, "Hello~")
+		if err != nil {
+			return
+		}
+	} else if r.URL.Path == "/about" {
+		fmt.Fprint(w, "此博客是用以记录编程笔记，请联系"+
+			"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
+	} else {
+		fmt.Fprint(w, "no pages")
+	}
 }
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
+
+	http.HandleFunc("/", handler)
 	http.ListenAndServe(":3000", nil)
+
 }
